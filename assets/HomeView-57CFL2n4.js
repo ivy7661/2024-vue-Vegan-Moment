b0265ec7af2ec0f1d730de375342c0b665907bb4
Ivy <ivy7661@gmail.com>
2024-02-20 15:41:16 +0800

    Updates

--- assets/HomeView-57CFL2n4.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,o as t,a as e}from"./index-bEA5hXIe.js";const c={components:{}},n={class:"home"},_=e("h1",null,"Hello, This is Home Page.",-1),a=e("hr",null,null,-1),r=[_,a];function l(i,d,h,p,m,u){return t(),s("div",n,r)}const $=o(c,[["render",l]]);export{$ as default};
--- assets/OrderView-PiN0v-lR.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as r}from"./index-bEA5hXIe.js";const n={};function o(t,s){return r(),c("h2",null,"訂單列表")}const a=e(n,[["render",o]]);export{a as default};
