c4694ba816c654d97d8ce391a95fa22728553935
Ivy <ivy7661@gmail.com>
2024-02-29 15:45:05 +0800

    商品card

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -46,172 +46,48 @@
         </div>
       </div>
     </div>
-    <section class="row justify-content-center mb-10">
-      <div class="col-12">
-        <!-- 產品 nav-tabs-content-->
-        <div class="tab-content" id="myTabContent">
-          <!-- 全部商品 -->
-          <div class="tab-pane fade show active" id="all" role="tabpanel" aria-labelledby="all-tab">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-5 mb-5">
-              <template v-for="product in displayedProducts" :key="product.id">
-                <div v-if="products" class="col card-hover">
-                  <div class="position-relative hover-show-btn">
-                    <div class="hover-img-mask">
-                      <RouterLink :to="`/product/${product.id}`" class="hover-img-mask">
-                        <img
-                          :src="product.imageUrl"
-                          :alt="product.title"
-                          class="w-100 d-block rounded-3 object-fit-cover"
-                          height="240"
-                        />
-                      </RouterLink>
-                      <button
-                        type="button"
-                        class="add-to-cart-btn btn btn-primary position-absolute bottom-0 start-50 translate-middle py-3 fs-5"
-                        :disabled="loadingStatus === product.id"
-                        @click="addToCart(product.id)"
-                      >
-                        <i
-                          class="fas fa-spinner fa-pulse me-2"
-                          v-if="loadingStatus === product.id"
-                        ></i>
-                        <span>加入購物車</span>
-                      </button>
-                    </div>
-                  </div>
-                  <RouterLink :to="`/product/${product.id}`">
-                    <div class="py-3 py-lg-4">
-                      <h4 class="fs-5 fs-lg-4 text-dark mb-2 mb-lg-3">{{ product.title }}</h4>
-                      <h5 class="text-primary d-flex align-items-center">
-                        NT${{ product.price }}
-                        <span class="fs-6 text-gray-dark ms-2"
-                          ><del>NT${{ product.origin_price }}</del></span
-                        >
-                      </h5>
-                    </div>
-                  </RouterLink>
-                </div>
-              </template>
+    <!-- 商品 -->
+    <section>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-5 my-1">
+        <div v-for="product in products" :key="product.id">
+          <div class="col card-hover">
+            <div class="position-relative hover-show-btn">
+              <div class="card-img">
+                <RouterLink :to="`/products/${product.id}`">
+                  <img
+                    :src="product.imageUrl"
+                    :alt="product.title"
+                    class="w-100 d-block rounded-3"
+                    height="240"
+                  />
+                </RouterLink>
+                <button
+                  type="button"
+                  class="add-to-cart-btn btn btn-primary position-absolute bottom-0 start-50 translate-middle py-3 fs-5"
+                  :disabled="loadingStatus === product.id"
+                  @click="addToCart(product.id)"
+                >
+                  <i class="fas fa-spinner fa-pulse me-2" v-if="loadingStatus === product.id"></i>
+                  <span>加入購物車</span>
+                </button>
+              </div>
             </div>
-            <ProductsPagination></ProductsPagination>
-          </div>
-          <!-- 超飽足果昔盆、輕食冷盤、湯品、裸食甜點、果昔飲品 -->
-          <div
-            v-for="tab in categoriesTabs"
-            :key="tab"
-            class="tab-pane fade"
-            :id="tab[1]"
-            role="tabpanel"
-            :aria-labelledby="`${tab[1]}-tab`"
-          >
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-5">
-              <template v-for="product in products" :key="product.id">
-                <div v-if="product.selectCategories === tab[0]" class="col card-hover">
-                  <div class="position-relative hover-show-btn">
-                    <div class="hover-img-mask">
-                      <RouterLink :to="`/product/${product.id}`" class="hover-img-mask">
-                        <img
-                          :src="product.imageUrl"
-                          :alt="product.title"
-                          class="w-100 d-block rounded-3 object-fit-cover"
-                          height="240"
-                        />
-                      </RouterLink>
-                      <button
-                        type="button"
-                        class="add-to-cart-btn btn btn-primary position-absolute bottom-0 start-50 translate-middle py-3 fs-5"
-                        :disabled="loadingStatus === product.id"
-                        @click="addToCart(product.id)"
-                      >
-                        <i
-                          class="fas fa-spinner fa-pulse me-2"
-                          v-if="loadingStatus === product.id"
-                        ></i>
-                        <span>加入購物車</span>
-                      </button>
-                    </div>
-                  </div>
-                  <RouterLink :to="`/product/${product.id}`">
-                    <div class="py-3 py-lg-4">
-                      <h5 class="mb-2 mb-lg-3">
-                        <span v-for="feature in product.checkboxFeatures" :key="feature">
-                          <span
-                            v-if="feature === '純素'"
-                            class="badge fs-6 bg-primary-light text-primary px-2 py-1 me-1 me-lg-2"
-                            >{{ feature }}</span
-                          >
-                          <span
-                            v-if="feature === '有機'"
-                            class="badge fs-6 px-2 py-1 me-1 me-lg-2"
-                            style="color: #2d9449; background-color: #ecf8f4"
-                            >{{ feature }}</span
-                          >
-                          <span
-                            v-if="feature === '無麩質'"
-                            class="badge fs-6 px-2 py-1 me-1 me-lg-2"
-                            style="color: #817447; background-color: #f8efec"
-                            >{{ feature }}</span
-                          >
-                          <span
-                            v-if="feature === '辣'"
-                            class="badge fs-6 px-2 py-1"
-                            style="color: #814747; background-color: #f8ecec"
-                            >{{ feature }}</span
-                          >
-                        </span>
-                      </h5>
-                      <h4 class="fs-5 fs-lg-4 text-dark mb-2 mb-lg-3">{{ product.title }}</h4>
-                      <h5 class="text-primary d-flex align-items-center">
-                        NT${{ $filters.toThousands(product.price) }}
-                        <span class="fs-6 text-gray-dark ms-2"
-                          ><del>NT${{ $filters.toThousands(product.origin_price) }}</del></span
-                        >
-                      </h5>
-                    </div>
-                  </RouterLink>
-                </div>
-              </template>
-            </div>
+            <RouterLink :to="`/product/${product.id}`">
+              <div class="py-3 py-lg-4">
+                <h4 class="fs-5 fs-lg-4 text-dark mb-2 mb-lg-3">{{ product.title }}</h4>
+                <h5 class="text-primary d-flex align-items-center">
+                  NT${{ product.price }}
+                  <span class="fs-6 text-gray-dark ms-2"
+                    ><del>NT${{ product.origin_price }}</del></span
+                  >
+                </h5>
+              </div>
+            </RouterLink>
           </div>
         </div>
       </div>
+      <ProductsPagination></ProductsPagination>
     </section>
-  </div>
-
-  <!-- 舊版 -->
-  <div class="col card-hover" v-for="product in products" v-bind:key="product.id">
-    <div class="position-relative hover-show-btn">
-      <div class="hover-img-mask">
-        <RouterLink :to="`/products/${product.id}`" class="hover-img-mask">
-          <img
-            :src="product.imageUrl"
-            :alt="product.title"
-            class="w-100 d-block rounded-3 object-fit-cover"
-            height="240"
-          />
-        </RouterLink>
-        <button
-          type="button"
-          class="add-to-cart-btn btn btn-primary position-absolute bottom-0 start-50 translate-middle py-3 fs-5"
-          :disabled="loadingStatus === product.id"
-          @click="addToCart(product.id)"
-        >
-          <i class="fas fa-spinner fa-pulse me-2" v-if="loadingStatus === product.id"></i>
-          <span>加入購物車</span>
-        </button>
-      </div>
-    </div>
-    <RouterLink :to="`/product/${product.id}`">
-      <div class="py-3 py-lg-4">
-        <h4 class="fs-5 fs-lg-4 text-dark mb-2 mb-lg-3">{{ product.title }}</h4>
-        <h5 class="text-primary d-flex align-items-center">
-          NT${{ product.price }}
-          <span class="fs-6 text-gray-dark ms-2"
-            ><del>NT${{ product.origin_price }}</del></span
-          >
-        </h5>
-      </div>
-    </RouterLink>
   </div>
 
   <h2>產品列表</h2>
@@ -318,5 +194,16 @@ export default {
 .router-link.active {
   color: #698f39;
   border-bottom: 4px solid #698f39;
+}
+
+.card-img {
+  overflow: hidden;
+  border-radius: 10px;
+  img {
+    transition: all 0.6s ease;
+    &:hover {
+      transform: scale(1.15);
+    }
+  }
 }
 </style>
