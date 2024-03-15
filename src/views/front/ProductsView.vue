1be7e554dc2a8b283b570b7d76ee69f75aab27ae
Ivy <ivy7661@gmail.com>
2024-03-15 16:26:42 +0800

    category fin

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -10,16 +10,6 @@
     </div>
   </section> -->
   <div class="container">
-    <!-- 舊的 -->
-    <!-- <router-link
-      v-for="category in categories"
-      :key="category"
-      :to="`/products?category=${category}`"
-      class="d-inline-block fs-5 mx-2"
-      :class="{ tabActive: selected }"
-      @click="changeClass(category)"
-      >{{ category }}</router-link
-    > -->
     <!-- breadcrumb -->
     <nav>
       <ol class="breadcrumb mb-3 mt-3">
@@ -49,13 +39,13 @@
           <li v-for="tab in categories" class="nav-item" role="presentation" :key="tab">
             <a
               class="nav-link fs-5 px-1 mx-2"
-              :id="`${tab[1]}-tab`"
+              :id="`${tab}-tab`"
               data-bs-toggle="tab"
-              :data-bs-target="`#${tab[1]}`"
+              :data-bs-target="`#${tab}`"
               href="#"
               role="tab"
             >
-              {{ tab[0] }}
+              {{ tab }}
             </a>
           </li>
         </ul>
@@ -66,92 +56,84 @@
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-5 mb-5">
               <template v-for="product in displayedProducts" :key="product.id">
                 <div v-if="products" class="col card-hover">
-                  <div class="position-relative hover-show-btn">
-                    <div class="hover-img-mask">
-                      <RouterLink :to="`/product/${product.id}`" class="hover-img-mask">
+                  <!-- card -->
+                  <div class="col">
+                    <div class="card-img">
+                      <RouterLink :to="`/products/${product.id}`">
                         <img
                           :src="product.imageUrl"
                           :alt="product.title"
-                          class="w-100 d-block rounded-3 object-fit-cover"
+                          class="w-100 d-block rounded-3"
                           height="240"
                         />
                       </RouterLink>
-                      <button
-                        type="button"
-                        class="add-to-cart-btn btn btn-primary position-absolute bottom-0 start-50 translate-middle py-3 fs-5"
-                        :disabled="loadingStatus === product.id"
-                        @click="addToCart(product.id)"
-                      >
-                        <i
-                          class="fas fa-spinner fa-pulse me-2"
-                          v-if="loadingStatus === product.id"
-                        ></i>
-                        <span>加入購物車</span>
-                      </button>
                     </div>
-                  </div>
-                  <RouterLink :to="`/product/${product.id}`">
-                    <div class="py-3 py-lg-4">
-                      <h4 class="fs-5 text-dark mb-2 mb-lg-3">{{ product.title }}</h4>
+
+                    <div class="py-3 py-lg-2">
+                      <RouterLink :to="`/products/${product.id}`">
+                        <h4 class="fs-4 text-dark mb-2 mb-lg-1">{{ product.title }}</h4>
+                      </RouterLink>
                       <h5 class="text-primary d-flex align-items-center">
-                        NT${{ $filters.currency(product.price) }}
-                        <span class="fs-6 text-gray-dark ms-2"
-                          ><del>NT${{ $filters.currency(product.origin_price) }}</del></span
+                        NT${{ product.price }}
+                        <span class="fs-6 text-gray-600 ms-2"
+                          ><del>NT${{ product.origin_price }}</del></span
                         >
                       </h5>
                     </div>
-                  </RouterLink>
+                    <!-- 購物車 btn -->
+                    <button
+                      type="button"
+                      class="btn btn-secondary fs-6 w-100"
+                      @click="addToCart(product.id, cartQty)"
+                    >
+                      <!-- :disabled="loadingStatus === product.id" -->
+                      <span>加入購物車</span>
+                    </button>
+                  </div>
                 </div>
               </template>
             </div>
             <ProductsPagination></ProductsPagination>
           </div>
           <!-- 超飽足果昔盆、輕食冷盤、湯品、裸食甜點、果昔飲品 -->
-          <div
-            v-for="tab in categories"
-            :key="tab"
-            class="tab-pane fade"
-            :id="tab[1]"
-            role="tabpanel"
-          >
+          <div v-for="tab in categories" :key="tab" class="tab-pane fade" :id="tab" role="tabpanel">
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 gy-5">
               <template v-for="product in products" :key="product.id">
-                <div v-if="product.category === tab[0]" class="col card-hover">
-                  <div class="position-relative hover-show-btn">
-                    <div class="hover-img-mask">
-                      <RouterLink :to="`/product/${product.id}`" class="hover-img-mask">
+                <div v-if="product.category === tab" class="col card-hover">
+                  <!-- card -->
+                  <div class="col">
+                    <div class="card-img">
+                      <RouterLink :to="`/products/${product.id}`">
                         <img
                           :src="product.imageUrl"
                           :alt="product.title"
-                          class="w-100 d-block rounded-3 object-fit-cover"
+                          class="w-100 d-block rounded-3"
                           height="240"
                         />
                       </RouterLink>
-                      <button
-                        type="button"
-                        class="add-to-cart-btn btn btn-primary position-absolute bottom-0 start-50 translate-middle py-3 fs-5"
-                        :disabled="loadingStatus === product.id"
-                        @click="addToCart(product.id)"
-                      >
-                        <i
-                          class="fas fa-spinner fa-pulse me-2"
-                          v-if="loadingStatus === product.id"
-                        ></i>
-                        <span>加入購物車</span>
-                      </button>
                     </div>
-                  </div>
-                  <RouterLink :to="`/product/${product.id}`">
-                    <div class="py-3 py-lg-4">
-                      <h4 class="fs-5 text-dark mb-2 mb-lg-3">{{ product.title }}</h4>
+
+                    <div class="py-3 py-lg-2">
+                      <RouterLink :to="`/products/${product.id}`">
+                        <h4 class="fs-4 text-dark mb-2 mb-lg-1">{{ product.title }}</h4>
+                      </RouterLink>
                       <h5 class="text-primary d-flex align-items-center">
-                        NT${{ $filters.currency(product.price) }}
-                        <span class="fs-6 text-gray-dark ms-2"
-                          ><del>NT${{ $filters.currency(product.origin_price) }}</del></span
+                        NT${{ product.price }}
+                        <span class="fs-6 text-gray-600 ms-2"
+                          ><del>NT${{ product.origin_price }}</del></span
                         >
                       </h5>
                     </div>
-                  </RouterLink>
+                    <!-- 購物車 btn -->
+                    <button
+                      type="button"
+                      class="btn btn-secondary fs-6 w-100"
+                      @click="addToCart(product.id, cartQty)"
+                    >
+                      <!-- :disabled="loadingStatus === product.id" -->
+                      <span>加入購物車</span>
+                    </button>
+                  </div>
                 </div>
               </template>
             </div>
@@ -212,12 +194,7 @@ export default {
   data() {
     return {
       tempProduct: {},
-      // categories: ['主餐', '輕食', '果昔碗', '飲品'],
-      categories: [
-        ['主餐', 'main'],
-        ['輕食', 'salad'],
-        ['飲品', 'juice']
-      ],
+      categories: ['主餐', '輕食', '果昔碗', '飲品'],
       cartQty: 1
     };
   },
