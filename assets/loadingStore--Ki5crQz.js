1207d6c243b7a3709245c78c7a9e6e0c51fb4c93
Ivy <ivy7661@gmail.com>
2024-03-18 15:52:23 +0800

    Updates

---
+++ assets/loadingStore--Ki5crQz.js
@@ -0,0 +1,1 @@
+import{H as o}from"./index-No_fbRuW.js";const e=o("loadingStore",{state:()=>({loadingProductId:""})});export{e as l};
--- assets/loadingStore-OZp-1xHn.js
+++
@@ -1,1 +0,0 @@
-import{H as o}from"./index-2HZypFVn.js";const e=o("loadingStore",{state:()=>({loadingProductId:""})});export{e as l};
