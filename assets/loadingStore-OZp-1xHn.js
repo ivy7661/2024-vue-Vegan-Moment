172ef224cf6242620d4703da191707de317d850d
Ivy <ivy7661@gmail.com>
2024-03-18 15:27:54 +0800

    Updates

---
+++ assets/loadingStore-OZp-1xHn.js
@@ -0,0 +1,1 @@
+import{H as o}from"./index-2HZypFVn.js";const e=o("loadingStore",{state:()=>({loadingProductId:""})});export{e as l};
---
+++ assets/swal-mJXYrnQA.js
@@ -0,0 +1,1 @@
+import{S as r}from"./sweetalert2.all-yx1le-Fi.js";const e={toastTop(t,o){return r.fire({title:t,icon:o,toast:!0,position:"top",showConfirmButton:!1,timer:1500,width:280})}};export{e as A};
