7a404d569930ba8d5120fa46c88619f0da6b1b69
Ivy <ivy7661@gmail.com>
2024-03-19 13:49:11 +0800

    Updates

--- assets/Logo-Vegan-Moment-Nj0AgRUB.js
+++
@@ -1,1 +0,0 @@
-const e="/HW-Vue-week6/assets/Logo-Vegan-Moment-Ym1dBS2_.png";export{e as _};
--- assets/swal-6HxkfyAW.js
+++
@@ -1,1 +0,0 @@
-import{S as r}from"./sweetalert2.all-15ItP-RY.js";const e={toastTop(t,o){return r.fire({title:t,icon:o,toast:!0,position:"top",showConfirmButton:!1,timer:1500,width:280})}};export{e as A};
